7bc97c2506fefad05da0a48efc1e25913a652fc3
Nick Morris <nickmorri@gmail.com>
2015-02-09 13:09:43 -0800

    Removed friends from production

--- app/app.js
+++ app/app.js
@@ -1,4 +1,4 @@
-var courseeater_app = angular.module('CourseEaterApp', ['ui.router', 'courseeater.auth', 'courseeater.track', 'courseeater.schedule', 'courseeater.friends', 'courseeater.settings']);
+var courseeater_app = angular.module('CourseEaterApp', ['ui.router', 'courseeater.auth', 'courseeater.track', 'courseeater.schedule', 'courseeater.settings']);
 
 courseeater_app.config(['$locationProvider', '$stateProvider', '$urlRouterProvider', function($locationProvider, $stateProvider, $urlRouterProvider) {
     
