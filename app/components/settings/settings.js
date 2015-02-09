fa56f07dbb2dc5c394551d4a1b44929d1dc4e324
Nick Morris <nickmorri@gmail.com>
2015-02-09 14:35:06 -0800

    Implemented password update

--- app/components/settings/settings.js
+++ app/components/settings/settings.js
@@ -10,24 +10,57 @@ settings.controller('SettingsController', ['$scope', 'AuthService', '$state', fu
         console.log($scope.newEmail);
     };
     
+    
+    $scope.passwordUpdateMessage = "";
     $scope.passwordUpdateError = false;
     $scope.currentPassword = undefined
     $scope.newPassword = undefined
     $scope.verifyPassword = undefined
     
     $scope.updatePassword = function () {
-        debugger;
-        
         if ($scope.newPassword != $scope.verifyPassword) {
             $scope.passwordUpdateError = true;
+            $scope.passwordUpdateSuccess = false;
+            $scope.passwordUpdateMessage = "New password and the verification password do not match. Please try again."
+            return;
         }
+        
+        if ($scope.newPassword == $scope.currentPassword) {
+            $scope.passwordUpdateError = true;
+            $scope.passwordUpdateSuccess = false;
+            $scope.passwordUpdateMessage = "New password and current password are the same. Please try entering a new password."
+            return;
+        }
+        
+        $scope.authService.checkLogin($scope.authService.currentUser.attributes.username, $scope.currentPassword).then(function (response) {
+            $scope.authService.currentUser.set("password", $scope.newPassword);
+            return $scope.authService.currentUser.save();
+        }).then(function (response) {
+            $scope.passwordUpdateSuccess = true;
+            
+            $scope.passwordUpdateMessage = "Password successfully updated!";
+            
+            $scope.passwordUpdateError = false;
+            $scope.currentPassword = undefined
+            $scope.newPassword = undefined
+            $scope.verifyPassword = undefined
+        }, function (error) {
+            $scope.passwordUpdateSuccess = false;
+            
+            $scope.passwordUpdateMessage = "Whoops! Something went wrong while updating your password. Please try again.";
+            
+            $scope.passwordUpdateError = true;
+            $scope.currentPassword = undefined
+            $scope.newPassword = undefined
+            $scope.verifyPassword = undefined
+        });
+        
     };
     
     $scope.deletePassword = undefined;
     
     $scope.deleteAccount = function () {
-        debugger
-        $scope.authService.login($scope.authService.currentUser.attributes.username, $scope.deletePassword).then(function (response) {
+        $scope.authService.checkLogin($scope.authService.currentUser.attributes.username, $scope.deletePassword).then(function (response) {
             return $scope.authService.currentUser.destroy();
         }).then(function () {
             $scope.authService.logout();
