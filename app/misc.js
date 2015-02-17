a268e054858a1b7b4d51c0a1b23b58ad4baf15bc
Nick Morris <nickmorri@gmail.com>
2015-02-16 21:33:01 -0800

    Added Google Analytics

--- app/misc.js
+++ app/misc.js
@@ -33,3 +33,15 @@ var getWeekday = function (day) {
 	date.setDate(thisMonday + day);
 	return date.toISOString().split("T")[0];;
 };
+
+var google_analytics = function () {
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+    
+    ga('create', 'UA-9939990-3', 'auto');
+    ga('send', 'pageview');
+};
+
+google_analytics();
