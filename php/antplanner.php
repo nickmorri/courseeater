95fbb1537d91acf4fbb2f7b6125281aa0518b567
Nick Morris <nickmorri@gmail.com>
2015-02-16 19:19:31 -0800

    Fixed antplanner data grabber

--- php/antplanner.php
+++ php/antplanner.php
@@ -1,18 +1,11 @@
 <?php
 
-$url = 'http://antplanner.appspot.com/schedule/load?username=test1';
-
+$url = 'http://antplanner.appspot.com/schedule/load?username=' + trim($_REQUEST['course_code']);
 
 $ch = curl_init();
 
 curl_setopt($ch, CURLOPT_URL, $url);
 
-
-// in real life you should use something like:
-// curl_setopt($ch, CURLOPT_POSTFIELDS, 
-//          http_build_query(array('postvar1' => 'value1')));
-
-// receive server response ...
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
 $server_output = curl_exec ($ch);
