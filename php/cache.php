e68c63961c386a1af93af8b7e17cfe3bd49ccd70
Nick Morris <nickmorri@gmail.com>
2015-02-23 21:28:13 -0800

    Cached items now live for 5 minutes

--- php/cache.php
+++ php/cache.php
@@ -1,6 +1,5 @@
 <?php
 
-
 function get_data($key) {
     $mem = new Memcached();
     $mem->addServer("127.0.0.1", 11211);
@@ -8,8 +7,9 @@ function get_data($key) {
 };
 
 function set_data($key, $data) {
+    $cache_life = 300;
     $mem = new Memcached();
     $mem->addServer("127.0.0.1", 11211);
-    return $mem->set($key, $data);
+    return $mem->set($key, $data, $cache_life);
 };
 
